7400782918595493320281dca3d87ea1edbc7ce9
Veslemøy <veslemoy.klavenes@gmail.com>
2026-06-03 07:45:51 +0200

    Add Meta Pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -25,8 +26,34 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Lora:wght@400;600;700;800&family=Inter:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        {/* Meta Pixel */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=840261168736940&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </head>
-      <body>{children}</body>
+      <body>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '840261168736940');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        {children}
+      </body>
     </html>
   )
 }
